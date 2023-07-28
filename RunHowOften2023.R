b44bdf74c4564b0fea4c2eb2b44b6bcc229b0f2b
hc3292 <hc3292@cumc.columbia.edu>
2023-07-28 13:13:37 -0400

    new_files

--- RunHowOften2023.R
+++ RunHowOften2023.R
@@ -5,10 +5,55 @@ library(devtools)
 # this installs the package from github
 devtools::install_github("hc3292/HowOften2023")
 
+library("devtools")
 library("SqlRender")
 library("DatabaseConnector")
 library("HowOften2023")
 
+connectionDetails <- createConnectionDetails(dbms= "sql server",
+                                             server = "gem.dbmi.columbia.edu",
+                                             user = "hc3292",
+                                             password = keyring::key_get("sql server", "hc3292"),
+                                             port="1433")
 
+###################################################################################
+###################################################################################
+################## CREATE TABLE OF SQL FILE OF DRUG EXPOSURES #####################
+###################################################################################
+###################################################################################
 
+rx_norms_list_sql = SqlRender::loadRenderTranslateSql(
+  "define_exposure_cohorts.sql",
+  "HowOften2023",
+  dbms = "sql server",
+  results_database_schema = "ohdsi_cumc_2022q4r1.results",
+  cdm_database_schema = "ohdsi_cumc_2022q4r1.dbo"
+)
 
+DatabaseConnector::disconnect(con)
+con <- DatabaseConnector::connect(connectionDetails)
+# this creates the IR_exposure_cohort table
+DatabaseConnector::executeSql(con,rx_norms_list_sql)
+
+# returns the exposure cohort into a dataframe
+exposure_cohort_sql = render("SELECT COHORT_DEFINITION_ID, COHORT_NAME FROM @a;", a = "ohdsi_cumc_2022q4r1.results.IR_cohort_definition")
+exposure_cohort = querySql(
+  connection = connect(connectionDetails),
+  sql = exposure_cohort_sql,
+  snakeCaseToCamelCase = FALSE
+)
+
+# clean up the table (underscores and colons will not work in the create sql function)
+replace_non_alphanumeric_with_underscore <- function(column_data) {
+  cleaned_column <- gsub("[^[:alnum:]]", "_", column_data)
+  return(cleaned_column)
+}
+# Apply the function to the column with non-alphanumeric characters
+exposure_cohort$COHORT_NAME <- replace_non_alphanumeric_with_underscore(exposure_cohort$COHORT_NAME)
+
+# create the sql files for the drugs
+result_vector <- mapply(create_exposure_sql, exposure_cohort$COHORT_DEFINITION_ID, exposure_cohort$COHORT_NAME)
+
+# append the names of the sql files to the exposure_cohort table
+
+# save it as a file called targetRef.csv
--- inst/sql/sql_server/define_exposure_cohorts.sql
+++ inst/sql/sql_server/define_exposure_cohorts.sql
@@ -1,10 +1,21 @@
+drop table if exists @results_database_schema.IR_cohort_definition;
+create table @results_database_schema.IR_cohort_definition
+(
+  how_often_id int IDENTITY(1,1),
+	cohort_definition_id bigint,
+	cohort_name varchar(500),
+	concept_id bigint,
+	cohort_type int,  --0: exposure, 1: outcome with 1st diagnosis; 2: outcome with 1st diagnosis + hospital
+)
+;
+
 --all exposure cohorts:  new users of drugs, newly diagnosed, 1yr washout
-IF OBJECT_ID('#exposure_cohort', 'U') IS NOT NULL
-	drop table #exposure_cohort;
+IF OBJECT_ID('@results_database_schema.IR_exposure_cohort', 'U') IS NOT NULL
+	drop table @results_database_schema.IR_exposure_cohort;
 
 --create table #exposure_cohort  as
 select de1.person_id as subject_id, de1.cohort_definition_id, de1.cohort_start_date, de1.cohort_end_date, op1.observation_period_end_date
-into #exposure_cohort
+into @results_database_schema.IR_exposure_cohort
 from
 (select person_id, drug_concept_id as cohort_definition_id, drug_era_start_date as cohort_start_date, drug_era_end_date as cohort_end_date, row_number() over (partition by person_id, drug_concept_id order by drug_era_start_date asc) rn1
 from @cdm_database_schema.drug_era
@@ -19,23 +30,12 @@ and de1.rn1 = 1
 ;
 
 insert into @results_database_schema.IR_cohort_definition (cohort_definition_id, cohort_name, concept_id, cohort_type)
-select e1.cohort_definition_id, 'New users of: ' + c1.concept_name as cohort_name, c1.concept_id, 0 as cohort_type
+select e1.cohort_definition_id, c1.concept_name as cohort_name, c1.concept_id, 0 as cohort_type
 from
 (
 select distinct cohort_definition_id
-from #exposure_cohort
+from @results_database_schema.IR_exposure_cohort
 ) e1
 inner join @cdm_database_schema.concept c1
 on e1.cohort_definition_id = c1.concept_id
 ;
-
-
---ToDo: should min_persons_exposed be 1000 or 10
-insert into @results_database_schema.IR_cohort_summary (cohort_definition_id, num_persons)
-select c1.cohort_definition_id,
-		count(c1.subject_id) as num_persons
-	from #exposure_cohort c1
-	group by c1.cohort_definition_id
-	having count(c1.subject_id) > 1000 --@min_persons_exposed
-;
-
