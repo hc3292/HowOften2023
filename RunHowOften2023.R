e4a660b91e52d79e2995384f5581a86459900102
hc3292 <hc3292@cumc.columbia.edu>
2023-07-28 17:55:16 -0400

    add_sql_files

--- RunHowOften2023.R
+++ RunHowOften2023.R
@@ -9,6 +9,7 @@ library("devtools")
 library("SqlRender")
 library("DatabaseConnector")
 library("HowOften2023")
+library("CohortGenerator")
 
 connectionDetails <- createConnectionDetails(dbms= "sql server",
                                              server = "gem.dbmi.columbia.edu",
@@ -55,5 +56,146 @@ exposure_cohort$COHORT_NAME <- replace_non_alphanumeric_with_underscore(exposure
 result_vector <- mapply(create_exposure_sql, exposure_cohort$COHORT_DEFINITION_ID, exposure_cohort$COHORT_NAME)
 
 # append the names of the sql files to the exposure_cohort table
+exposure_cohort$fileName = paste0(replace_non_alphanumeric_with_underscore(exposure_cohort$COHORT_NAME), ".sql")
+# generate a HowOftenID
+exposure_cohort$cohortId = c(1:nrow(exposure_cohort))
+# generate cohortName column
+exposure_cohort$cohortName = exposure_cohort$COHORT_NAME
 
 # save it as a file called targetRef.csv
+write.csv(exposure_cohort, "~/HowOften2023/inst/settings/targetRef.csv", quote = F, row.names = F)
+
+###################################################################################
+###################################################################################
+################## CREATE TABLE OF SQL FILE OF DRUG OUTCOMES  #####################
+###################################################################################
+###################################################################################
+
+outcomes_list_sql = SqlRender::loadRenderTranslateSql(
+  "define_outcome_cohorts.sql",
+  "HowOften2023",
+  dbms = "sql server",
+  results_database_schema = "ohdsi_cumc_2022q4r1.results",
+  cdm_database_schema = "ohdsi_cumc_2022q4r1.dbo"
+)
+
+DatabaseConnector::disconnect(con)
+con <- DatabaseConnector::connect(connectionDetails)
+# this creates the IR_exposure_cohort table
+DatabaseConnector::executeSql(con,outcomes_list_sql)
+
+# returns the exposure cohort into a dataframe
+outcome_cohort_sql = render("SELECT CONCEPT_ID, COHORT_NAME, COHORT_TYPE FROM @a;", a = "ohdsi_cumc_2022q4r1.results.IR_cohort_definition")
+outcome_cohort = querySql(
+  connection = connect(connectionDetails),
+  sql = outcome_cohort_sql,
+  snakeCaseToCamelCase = FALSE
+)
+
+outcome_first_dx = outcome_cohort[which(outcome_cohort$COHORT_TYPE == 1), ]
+
+# Apply the function to the column with non-alphanumeric characters
+outcome_first_dx$COHORT_NAME <- replace_non_alphanumeric_with_underscore(outcome_first_dx$COHORT_NAME)
+
+# create the sql files for the drugs
+result_vector <- mapply(create_outcome_sql, outcome_first_dx$CONCEPT_ID, outcome_first_dx$COHORT_NAME)
+
+# append the names of the sql files to the exposure_cohort table
+outcome_first_dx$fileName = paste0(replace_non_alphanumeric_with_underscore(outcome_first_dx$COHORT_NAME), ".sql")
+# generate a HowOftenID
+outcome_first_dx$cohortId = c( (nrow(exposure_cohort) + 1): ((nrow(exposure_cohort) + nrow(outcome_first_dx))))
+# generate cohortName column
+outcome_first_dx$cohortName = outcome_first_dx$COHORT_NAME
+outcome_first_dx$outcomeCohortDefinitionId = outcome_first_dx$cohortId
+outcome_first_dx$cleanWindow = 0 #idk if we need this
+outcome_first_dx$primaryTimeAtRiskStartOffset = 1
+outcome_first_dx$primaryTimeAtRiskStartIndex = 0
+outcome_first_dx$primaryTimeAtRiskEndOffset = 30
+outcome_first_dx$primaryTimeAtRiskEndIndex = 0
+
+
+# save it as a file called targetRef.csv
+write.csv(outcome_first_dx, "~/HowOften2023/inst/settings/outcomeRef.csv", quote = F, row.names = F)
+
+
+
+###################################################################################
+###################################################################################
+################## GENERATE TIME AT RISK CSV  #####################################
+###################################################################################
+###################################################################################
+
+# set up time at risk
+timeAtRisk = data.frame(rbind(c(1,0,0,30,0), c(2,0,0,365,0)))
+colnames(timeAtRisk) = c("time_at_risk_id", "time_at_risk_start_offset",
+                         "time_at_risk_start_index",
+                         "time_at_risk_end_offset", "time_at_risk_end_index")
+
+write.csv(timeAtRisk, "~/HowOften2023/inst/settings/timeAtRisk.csv", quote = F, row.names = F)
+
+
+###################################################################################
+###################################################################################
+################## INSTANTIATE COHORTS  ###########################################
+###################################################################################
+###################################################################################
+
+## create reference tables
+# the table names start with HowOften_[etcetc]
+
+cohortTablePrefix = "HowOften"
+targetCohortTable = paste0(cohortTablePrefix, "_target")
+targetRefTable = paste0(cohortTablePrefix, "_target_ref")
+subgroupCohortTable = paste0(cohortTablePrefix, "_subgroup")
+subgroupRefTable = paste0(cohortTablePrefix, "_subgroup_ref")
+outcomeCohortTable = paste0(cohortTablePrefix, "_outcome")
+outcomeRefTable = paste0(cohortTablePrefix, "_outcome_ref")
+timeAtRiskTable = paste0(cohortTablePrefix, "_time_at_risk")
+summaryTable = paste0(cohortTablePrefix, "_ir_summary")
+
+createRefTablesSql <- SqlRender::loadRenderTranslateSql("CreateRefTables.sql",
+                                                        packageName = "Covid19VaccineAesiIncidenceCharacterization",
+                                                        dbms = "sql server",
+                                                        tempEmulationSchema = NULL,
+                                                        warnOnMissingParameters = TRUE,
+                                                        cohort_database_schema = "ohdsi_cumc_2022q4r1.dbo",
+                                                        summary_table = summaryTable,
+                                                        target_ref_table = targetRefTable,
+                                                        subgroup_ref_table = subgroupRefTable,
+                                                        outcome_ref_table = outcomeRefTable,
+                                                        time_at_risk_table = timeAtRiskTable
+)
+DatabaseConnector::executeSql(connection = connect(connectionDetails),
+                              sql = createRefTablesSql,
+                              progressBar = TRUE,
+                              reportOverallTime = TRUE)
+
+
+## instantiate cohorts
+
+targetCohorts <- readCsv("inst/settings/targetRef.csv")
+# subgroupCohorts <- readCsv("settings/subgroupRef.csv") # ignore for now
+outcomeCohorts <- readCsv("inst/settings/outcomeRef.csv")
+timeAtRisk <- readCsv("inst/settings/timeAtRisk.csv")
+
+instantiatedTargetCohortIds <- c()
+
+incremental = TRUE
+incrementalFolder = "incrementalFolder"
+cohortDatabaseSchema = "ohdsi_cumc_2022q4r1.dbo"
+cdmDatabaseSchema = "ohdsi_cumc_2022q4r1.dbo"
+tempEmulationSchema = "ohdsi_cumc_2022q4r1.dbo"
+connection = connect(connectionDetails)
+
+
+instantiatedTargetCohortIds <- instantiateCohortSet(connectionDetails = connectionDetails,
+                                                    connection = connect(connectionDetails),
+                                                    cdmDatabaseSchema = cdmDatabaseSchema,
+                                                    tempEmulationSchema = tempEmulationSchema,
+                                                    cohortDatabaseSchema = cohortDatabaseSchema,
+                                                    cohortTable = targetCohortTable,
+                                                    cohorts = targetCohorts,
+                                                    cohortSqlFolder = "target",
+                                                    createCohortTable = TRUE,
+                                                    incremental = incremental,
+                                                    incrementalFolder = incrementalFolder)
--- inst/sql/sql_server/define_outcome_cohorts.sql
+++ inst/sql/sql_server/define_outcome_cohorts.sql
@@ -1,7 +1,23 @@
---define set of concepts to be used as eligble concepts for the adverse reactions
+drop table if exists @results_database_schema.IR_cohort_definition;
+create table @results_database_schema.IR_cohort_definition
+(
+  how_often_id int IDENTITY(1,1),
+	cohort_definition_id bigint,
+	cohort_name varchar(500),
+	concept_id bigint,
+	cohort_type int,  --0: exposure, 1: outcome with 1st diagnosis; 2: outcome with 1st diagnosis + hospital
+)
+;
 
+
+
+
+-- OUTCOMES
+--define set of concepts to be used as eligble concepts
 IF OBJECT_ID('#concept_anc_group', 'U') IS NOT NULL
 	drop table #concept_anc_group;
+
+--drop table #concept_anc_group;
 
 --create a temp table of concepts to aggregate to:
 --create table #concept_anc_group  as
@@ -35,4 +51,93 @@ and c1.concept_name not in ('Disease','Clinical history and observation findings
 and c1.domain_id = 'Condition'
 ) t1
 on ca1.ancestor_concept_id = t1.concept_id
+;
+
+
+--select count(*) from #concept_anc_group;
+
+
+--outcome cohorts 1:  first diagnosis of any sort
+IF OBJECT_ID('#outcome_cohort_1', 'U') IS NOT NULL
+	drop table #outcome_cohort_1;
+
+
+-- drop table #outcome_cohort_1;
+
+
+--create table #outcome_cohort_1  as
+select t1.person_id as subject_id, cast(t1.ancestor_concept_id as bigint)*100+1 as cohort_definition_id, t1.cohort_start_date, t1.cohort_start_date as cohort_end_date
+into #outcome_cohort_1
+from
+(
+select co1.person_id, ca1.ancestor_concept_id, min(co1.condition_start_date) as cohort_start_date
+from @cdm_database_schema.condition_occurrence co1
+inner join #concept_anc_group ca1
+on co1.condition_concept_id = ca1.descendant_concept_id
+group by co1.person_id, ca1.ancestor_concept_id
+) t1
+;
+
+
+--outcome cohorts 2:  first diagnosis of a condition that is observed at hospital at some point
+IF OBJECT_ID('#outcome_cohort_2', 'U') IS NOT NULL
+	drop table #outcome_cohort_2;
+
+-- drop table #outcome_cohort_2;
+--create table #outcome_cohort_2  as
+select t1.person_id as subject_id, cast(t1.ancestor_concept_id as bigint)*100+2 as cohort_definition_id, t1.cohort_start_date, t1.cohort_start_date as cohort_end_date
+into #outcome_cohort_2
+from
+(
+select co1.person_id, ca1.ancestor_concept_id, min(co1.condition_start_date) as cohort_start_date
+from @cdm_database_schema.condition_occurrence co1
+inner join #concept_anc_group ca1
+on co1.condition_concept_id = ca1.descendant_concept_id
+group by co1.person_id, ca1.ancestor_concept_id
+) t1
+inner join
+(
+select co1.person_id, ca1.ancestor_concept_id, min(vo1.visit_start_date) as cohort_start_date
+from @cdm_database_schema.condition_occurrence co1
+inner join @cdm_database_schema.visit_occurrence vo1
+on co1.person_Id = vo1.person_id
+and co1.visit_occurrence_id = vo1.visit_occurrence_id
+and visit_concept_id = 9201
+inner join #concept_anc_group ca1
+on co1.condition_concept_id = ca1.descendant_concept_id
+group by co1.person_id, ca1.ancestor_concept_id
+) t2
+on t1.person_id = t2.person_id
+and t1.ancestor_concept_id = t2.ancestor_concept_id
+;
+
+
+--outcome cohorts:  combine both types together
+IF OBJECT_ID('@results_database_schema.IR_outcome_cohort', 'U') IS NOT NULL
+	drop table @results_database_schema.IR_outcome_cohort;
+
+--create table #outcome_cohort  as
+select t1.* into @results_database_schema.IR_outcome_cohort
+from
+(
+select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date
+from #outcome_cohort_1
+
+union
+
+select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date
+from #outcome_cohort_2
+) t1
+;
+
+
+insert into @results_database_schema.IR_cohort_definition (cohort_definition_id, cohort_name, concept_id, cohort_type)
+select e1.cohort_definition_id, case when right(e1.cohort_definition_id, 1) = '1' then 'First diagnosis of: ' when right(e1.cohort_definition_id, 1) = '2' then 'First diagnosis and >=1 hospitalization with: ' else 'Other type of: ' end + c1.concept_name as cohort_name, c1.concept_id, right(e1.cohort_definition_id, 1) as cohort_type
+from
+(
+select distinct cohort_definition_id
+from @results_database_schema.IR_outcome_cohort
+) e1
+inner join @cdm_database_schema.concept c1
+on left(e1.cohort_definition_id, len(e1.cohort_definition_id)-2) = c1.concept_id
 ;
